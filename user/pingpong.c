e09abd623ea5019cbd5506c72f75f69f293a5320
wrdowney <willd20@vt.edu>
2022-12-30 11:39:42 -0500

    add error checking to pingpong

--- user/pingpong.c
+++ user/pingpong.c
@@ -6,25 +6,46 @@ int
 main(int argc, char *argv[]){
     int p[2];
     pipe(p);
+    
+    int n;
 
     if(fork() == 0) { //child 
         //read byte from parent
         char buf[1];
-        read(p[0], buf, 1);
+
+        n = read(p[0], buf, 1);
+        if(n < 0) {
+            fprintf(2, "read error\n");
+            exit(1);
+        }
+
         if(buf[0] == '1')
             printf("%d: received ping\n", getpid());
         
         //send byte to parent
-        write(p[1], "1", 1);
+        if(write(p[1], "1", 1) != 1) {
+            fprintf(2, "write error\n");
+            exit(1);
+        }
+        exit(0);
     } else { //parent 
         //send byte to child
-        write(p[1], "1", 1);
+        if(write(p[1], "1", 1) != 1) {
+            fprintf(2, "write error\n");
+            exit(1);
+        }
 
         //read byte from child
         char buf[1];
-        read(p[0], buf, 1);
+
+        n = read(p[0], buf, 1);
+        if(n < 0) {
+            fprintf(2, "read error\n");
+            exit(1);
+        }
+
         if(buf[0] == '1')
             printf("%d: received pong\n", getpid());
+        exit(0);
     }
-    return 0;
 }
